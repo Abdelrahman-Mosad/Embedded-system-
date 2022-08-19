7cd002728c771a5b57153f3c8b438df7786f6364
Abdelrahman Mosad <109417929+Abdo-Mosad@users.noreply.github.com>
2022-08-19 23:44:27 +0200

    Create EX_7.c

---
+++ Unit_2_C_programming/Assignments/H.W_1/EX_7.c
@@ -0,0 +1,20 @@
+  #include <stdio.h>
+int main()
+{
+    float num1, num2;
+    printf("Enter first number: ");
+    fflush(stdout); fflush(stdin);
+    scanf("%f",&num1);
+    printf("Enter second number: ");
+    fflush(stdout); fflush(stdin);
+    scanf("%f", &num2 );
+
+   num1 = num1 - num2;
+   num2 = num1 + num2;
+   num1 = num2 - num1; 
+
+    printf("After swapping, first number= %f\n",num1);
+    printf("After swapping, second number= %f",num2);
+
+    return 0;
+}
