e21ce1292041a8b334b09b509688661cdd8c0ec4
Abdelrahman Mosad <109417929+Abdo-Mosad@users.noreply.github.com>
2022-08-21 00:19:31 +0200

    Create EX_8.c

---
+++ Unit_2_c_programing/Assignments/HW_2/EX_8.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+int main(){
+
+    char s;
+    float  num1,num2;
+    printf("choose an operator either * or / or + or -: ");
+    scanf("%c",&s);
+
+    printf("Enter two operands: ");
+    scanf("%f%f",&num1,&num2);
+    fflush(stdout); fflush(stdin);
+
+    switch(s){
+        case '*':
+        printf("%.1f * %.1f = %.1f",num1,num2,num1*num2);
+        break;
+        case '/':
+        printf("%.1f / %.1f = %.1f",num1,num2,num1/num2);
+        break;
+        case '+':
+        printf("%.1f + %.1f = %.1f",num1,num2,num1+num2);
+        break;
+        case '-':
+        printf("%.1f - %.1f = %.1f",num1,num2,num1-num2);
+        break;
+    default:
+        printf("Error! operator is not correct");
+        break;    
+    }
+
+    return 0;
+
+}
