bcb8b9676230b60e36d8fa56eb5f5f34b6dfb5e1
Abdelrahman Mosad <109417929+Abdo-Mosad@users.noreply.github.com>
2022-08-21 00:02:10 +0200

    Create EX_7.c

---
+++ Unit_2_c_programing/Assignments/HW_2/EX_7.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+int main(){
+
+    int n, count;
+    unsigned long long int factorial=1;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    if(n<0)
+    printf("Error!!! Fcatorial of negative number does not exist ");
+    else
+    {
+        for(count=1;count<=n;++count)
+    {
+        factorial*=count;
+
+    }
+    printf("factorial= %lu", factorial);
+
+    }
+
+
+    return 0;
+
+}
