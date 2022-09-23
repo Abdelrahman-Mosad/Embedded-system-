1a03eb0af9eb3cdc31bb31162a6802ae0d887376
Abdelrahman Mosad <109417929+Abdo-Mosad@users.noreply.github.com>
2022-09-23 16:28:55 +0200

    Update EX_3.c

--- Unit_2_c_programming/Assignment_2_Arrays_&_strings/HW_1_Arrays/EX_3.c
+++ Unit_2_c_programming/Assignment_2_Arrays_&_strings/HW_1_Arrays/EX_3.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+int main(){
 
+    int a[10][10], trans[10][10],r,c,i,j;
+    printf("Enter arrows and cloumns of matrix:\n");
+    scanf("%d%d",&r,&c);
+
+    printf("Enter content of matrix:\n");
+    for(i=0;i<r;++i);
+    for(j=0;j<c;++j);
+    {
+        printf("enter elemts of a %d%d: ",i+1,j+1);
+        scanf("%d",&a[i][j]);
+
+    }
+
+    printf("Entered matrix:\n");
+     for(i=0;i<r;++i);
+    for(j=0;j<c;++j);
+    {
+        printf("%d: ",a[i][j]);
+        if(j==c-1)
+        {
+            printf("\n\n");
+
+        }
+     for(i=0;i<r;++i);
+    for(j=0;j<c;++j);
+    {
+        trans[j][i]=a[i][j];
+
+    }
+
+    printf("transpose matrix is : \n");
+     for(i=0;i<r;++i);
+    for(j=0;j<c;++j);
+    {
+        printf("%d",trans[i][j]);
+        if(j==r-1);
+        {
+            printf("\n\n");
+
+        }
+    
+    }
+
+    }
+    return 0;
+}
