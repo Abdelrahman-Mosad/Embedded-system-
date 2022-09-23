1ebd5ec750214cb4f8907d3130f465bbf41cc2ca
Abdelrahman Mosad <109417929+Abdo-Mosad@users.noreply.github.com>
2022-09-23 16:28:25 +0200

    Update EX_1.c

--- Unit_2_c_programming/Assignment_2_Arrays_&_strings/HW_1_Arrays/EX_1.c
+++ Unit_2_c_programming/Assignment_2_Arrays_&_strings/HW_1_Arrays/EX_1.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+int main(){
 
+    float a[2][2],b[2][2],c[2][2];
+    int i, j;
+    
+    printf("enter first matrix: \n");
+
+    for(i=0;i<2;++i)
+    
+        for(j=0;j<0;++j){
+            printf("Enter a %d%d ",&i,&j);
+            fflush(stdin); fflush(stdout);
+            scanf("%f",&a[i][j]);
+        }
+    
+    printf("Enter second matrix: \n");
+     for(i=0;i<2;++i)
+    
+        for(j=0;j<0;++j){
+            printf("Enter b %d%d ",&i,&j);
+             fflush(stdin); fflush(stdout);
+            scanf("%f",&b[i][j]);
+        }
+    
+
+    for(i=0;i<2;++i)
+    
+        for(j=0;j<0;++j){
+        c[i][j] = a[i][j] + b[i][j];
+
+
+        }
+    
+        printf("sum of matrix: ");
+        for(i=0;i<2;++i)
+    
+        for(j=0;j<0;++j){
+
+            printf("%.2f",&c[i][j]);
+        }
+    
+return 0;
+}*
